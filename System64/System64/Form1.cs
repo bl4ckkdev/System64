87f80b80b2c7b0a3393ad8375e3c2ef73fb77802
vp10gr <88006874+vp10gr@users.noreply.github.com>
2023-02-27 01:50:11 +0200

    xx
    zx

---
+++ System64/System64/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace System64
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			DialogResult firstwarning = MessageBox.Show("This virus is no joke, do you want to run?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+			if (firstwarning == DialogResult.Yes)
+			{
+				DialogResult secondwarning = MessageBox.Show("THIS IS THE LAST WARNING, IT WILL DESTROY THE BOOTLOADER WE ARE NOT RESPONSIBLE FOR ANY DAMAGES THIS IS JUST FOR FUN, ARE YOU SURE YOU WANT TO RUN IT?", "LAST WARNING", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+				if (secondwarning == DialogResult.Yes)
+				{
+
+				}
+				else if (secondwarning == DialogResult.No)
+				{
+					this.Close();
+					Application.Exit();
+				}
+			}
+			else if (firstwarning == DialogResult.No)
+			{
+				this.Close();
+				Application.Exit();
+			}
+		}
+	}
+}
